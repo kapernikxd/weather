7540b82552eb7bf262ecb940594d4ad7fb282845
kapernikxd <kapernikxd@gmail.com>
2021-01-31 11:00:35 +0300

    Установил  Vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueConfirmDialog from 'vue-confirm-dialog'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 Vue.use(VueConfirmDialog)
@@ -11,5 +12,6 @@ Vue.component('vue-confirm-dialog', VueConfirmDialog.default)
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
